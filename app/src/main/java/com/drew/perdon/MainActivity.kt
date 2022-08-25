dcdbdfb212d6dcb87a8f2b98fef537420f714b64
Sash0l <Sash0l@yandex.ru>
2022-08-25 23:28:39 +0500

    Add shake detection, play vine boom on shake

--- app/src/main/java/com/drew/perdon/MainActivity.kt
+++ app/src/main/java/com/drew/perdon/MainActivity.kt
@@ -9,11 +9,24 @@ import android.view.MotionEvent
 import android.view.MotionEvent.ACTION_DOWN
 import android.view.MotionEvent.ACTION_UP
 import android.widget.ImageView
+import android.widget.Toast
+import com.github.nisrulz.sensey.Sensey
+import com.github.nisrulz.sensey.ShakeDetector
 import kotlin.collections.HashMap
 
 private const val TAG = "MainActivity"
 private val soundPlayer = SoundPool.Builder().setMaxStreams(1).build()
 private val soundsDictionary = HashMap<String, Int>()
+
+val shakeListener = object : ShakeDetector.ShakeListener {
+    override fun onShakeDetected() {
+        Log.d(TAG, "Phone got shaken")
+        soundPlayer.play(soundsDictionary["boom"]!!, 1f, 1f, 1, 0, 1f)
+    }
+    override fun onShakeStopped() {
+        Log.d(TAG, "Shake stopped")
+    }
+}
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,14 +34,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         initializeSounds()
+        Sensey.getInstance().init(this)
+        Sensey.getInstance().startShakeDetection(20f, 300, shakeListener)
     }
 
+    override fun onDestroy() {
+        Sensey.getInstance().stop()
+        super.onDestroy()
+    }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         if (event?.action == ACTION_DOWN) {
             Log.d(TAG, "Screen got touched")
             slapTable()
-            playSound()
+            playSound("perdon")
         }
         if (event?.action == ACTION_UP) {
             unSlapTable()
@@ -45,19 +64,20 @@ class MainActivity : AppCompatActivity() {
             else ->
                 soundsDictionary["perdon"] = soundPlayer.load(this, R.raw.perdon_blanco, 1)
         }
+        soundsDictionary["boom"] = soundPlayer.load(this, R.raw.boom, 1)
         Log.d(TAG, "Sounds initialized")
     }
 
-    private fun playSound() {
+    private fun playSound(soundName: String) {
         soundPlayer.play(
-            soundsDictionary["perdon"]!!,
+            soundsDictionary[soundName]!!,
             1f,
             1f,
             1,
             0,
             1f
         )
-        Log.d(TAG, "Played perdon sound")
+        Log.d(TAG, "Played $soundName sound")
     }
 
     private fun slapTable() {
