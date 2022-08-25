3d72853a010ec5092fc522c7192b59d1eacf1630
Sash0l <Sash0l@yandex.ru>
2022-08-25 19:55:22 +0500

    Change sound according to phone's theme

--- app/src/main/java/com/drew/perdon/MainActivity.kt
+++ app/src/main/java/com/drew/perdon/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.drew.perdon
 
+import android.content.res.Configuration
 import android.media.SoundPool
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.MotionEvent
 import android.view.MotionEvent.ACTION_DOWN
+import android.view.MotionEvent.ACTION_UP
+import android.widget.ImageView
 import kotlin.collections.HashMap
 
 private const val TAG = "MainActivity"
@@ -24,28 +27,44 @@ class MainActivity : AppCompatActivity() {
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         if (event?.action == ACTION_DOWN) {
             Log.d(TAG, "Screen got touched")
+            slapTable()
             playSound()
+        }
+        if (event?.action == ACTION_UP) {
+            unSlapTable()
         }
         return super.onTouchEvent(event)
     }
 
     private fun initializeSounds() {
-        soundsDictionary["perdonNegro"] = soundPlayer.load(this, R.raw.perdon_negro, 1)
-        soundsDictionary["perdonBlanco"] = soundPlayer.load(this, R.raw.perdon_blanco, 1)
-        Log.d(TAG,"Sounds initialized")
+        val uiMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+        Log.d(TAG, "UI theme: $uiMode")
+        when (uiMode) {
+            Configuration.UI_MODE_NIGHT_YES ->
+                soundsDictionary["perdon"] = soundPlayer.load(this, R.raw.perdon_negro, 1)
+            else ->
+                soundsDictionary["perdon"] = soundPlayer.load(this, R.raw.perdon_blanco, 1)
+        }
+        Log.d(TAG, "Sounds initialized")
     }
 
     private fun playSound() {
-        //if phone has dark theme enabled:
         soundPlayer.play(
-            soundsDictionary["perdonNegro"]!!,
+            soundsDictionary["perdon"]!!,
             1f,
             1f,
             1,
             0,
             1f
         )
-        Log.d(TAG, "Played perdonNegro sound")
-        //TODO: if phone has light theme enabled:
+        Log.d(TAG, "Played perdon sound")
+    }
+
+    private fun slapTable() {
+        findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.hand_on_table)
+    }
+
+    private fun unSlapTable() {
+        findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.hand_in_air)
     }
 }
